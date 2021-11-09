47a078b338925292a7ed2ba756c5935cd316ecb0
serhat-kose <serhatkse94@gmail.com>
2021-11-09 16:18:03 +0300

    updateBook and listBook methods modifying

--- client/book-store/src/components/Book.js
+++ client/book-store/src/components/Book.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Card, Form, Button, Col, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusSquare, faSave, faUndo } from "@fortawesome/free-solid-svg-icons";
+import { faPlusSquare, faSave, faUndo,faList,faEdit } from "@fortawesome/free-solid-svg-icons";
 import axios from 'axios'
 import Toaster from "./Toast";
 // import { Container, Navbar,Col } from "react-bootstrap";
@@ -17,8 +17,34 @@ export default class Book extends Component {
 	}
 
 	initialState = {
-		title:'',author:'',coverPhotoUrl:'',isbnNumber:'',price:'',language:''
+		id:'',title:'',author:'',coverPhotoUrl:'',isbnNumber:'',price:'',language:''
 	}
+
+  componentDidMount( ){
+    const bookId= this.props.match.params.id;
+
+    if(bookId){
+        this.findBookById(bookId);
+    }
+  }
+
+  findBookById = (bookId) => {
+    axios.get("http://localhost:8080/api/v1/books/"+bookId).then(response=>{
+      if(response.data!=null){
+       this.setState({
+         id:response.data.id,
+         title:response.data.title,
+         author:response.data.author,
+         coverPhotoUrl:response.data.coverPhotoUrl,
+         isbnNumber:response.data.isbnNumber,
+         price:response.data.price,
+         language:response.data.language
+       })
+      }
+    }).catch((error)=>{
+        console.log("Error :" + error)
+    })
+  }
 
 	submitBook = event =>{
 
@@ -34,7 +60,7 @@ export default class Book extends Component {
 			language : this.state.language
 		}
 
-		axios.post("http://localhost:8080/api/v1/books",book).then(response =>{
+		axios.post("http://localhost:8080/api/v1/books/add",book).then(response =>{
 				if(response.data!=null){
 					
 					this.setState({"show":true})
@@ -46,6 +72,32 @@ export default class Book extends Component {
 		this.setState(this.initialState)
 		
 	}
+
+  updateBook = (event) => {
+    event.preventDefault();
+		
+		
+		const book = {
+      id: this.state.id,
+			title : this.state.title ,
+			author :  this.state.author ,
+			coverPhotoUrl : this.state.coverPhotoUrl,
+			isbnNumber : this.state.isbnNumber,
+			price : this.state.price,
+			language : this.state.language
+		}
+
+		axios.put("http://localhost:8080/api/v1/books/update",book).then(response =>{
+				if(response.data!=null){
+					
+					this.setState({"show":true})
+					setTimeout(() => this.setState({"show":false}), 3000);
+					
+				}
+		})
+
+		this.setState(this.initialState)
+  }
 
 	resetBook = () =>{
 
@@ -61,6 +113,10 @@ export default class Book extends Component {
 		
 	}
 
+  bookList = () => {
+    return this.props.history.push("/list")
+  }
+
   render() {
 
 	const {title,author,coverPhotoUrl,isbnNumber,price,language} = this.state;
@@ -70,8 +126,8 @@ export default class Book extends Component {
 				<Toaster show={this.state.show} message={"Book Saved Successfully."} type={"success"} ></Toaster>
 			</div>
 			<Card className="border border-dark bg-dark text-white">
-        <Card.Header><FontAwesomeIcon icon={ faPlusSquare} /> Add Book </Card.Header>
-        <Form onReset={this.resetBook} onSubmit={this.submitBook} id="bookFormId">
+        <Card.Header><FontAwesomeIcon icon={ this.state.id ? faEdit : faPlusSquare} /> {this.state.id ? "Update Book" : "Add Book"} </Card.Header>
+        <Form onReset={this.resetBook} onSubmit={this.state.id ? this.updateBook : this.submitBook} id="bookFormId">
           <Card.Body>
             <Row>
               <Form.Group as={Col} controlId="formGridTitle">
@@ -153,12 +209,16 @@ export default class Book extends Component {
           </Card.Body>
           <Card.Footer style={{ textAlign: "right" }}>
             <Button size="sm" variant="success" type="submit">
-			<FontAwesomeIcon icon={faSave} /> {' '}
-               Save
+			<FontAwesomeIcon icon={faSave} /> {this.state.id ? "Update" : "Save"}
+               
             </Button> {' '}
 			<Button size="sm" variant="info" type="reset">
-			<FontAwesomeIcon icon={faUndo} /> {' '}
+			<FontAwesomeIcon icon={faUndo} /> 
                Reset
+            </Button>{' '}
+            <Button size="sm" variant="info" type="button" onClick={this.bookList.bind()}>
+			<FontAwesomeIcon icon={faList} /> 
+               Book List
             </Button>
           </Card.Footer>
         </Form>
